008c165379ddafbca21c911b7e19b8912ba7005f
moriony <demateus@gmail.com>
2015-11-05 15:34:12 +0300

    rename example file

--- examples/create_calculator_via_facotry.php
+++ examples/create_calculator_via_facotry.php
@@ -5,6 +5,6 @@ include_once __DIR__.'/../vendor/autoload.php';
 $config = include __DIR__.'/../src/Resources/Asendia/PMI/index.php';
 
 $factory = new \EsteIt\PackageDeliveryCalculator\Factory\AsendiaCalculatorFactory();
-$deliveryMethod = $factory->create($config);
+$calculator = $factory->create($config);
 
-var_dump($deliveryMethod);
+var_dump($calculator);
