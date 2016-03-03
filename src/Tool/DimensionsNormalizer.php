84a760525df329f1ca903a7e1e3eb733350a8d18
moriony <demateus@gmail.com>
2016-03-03 10:41:42 +0300

    fix dimensions normalizer

--- src/Tool/DimensionsNormalizer.php
+++ src/Tool/DimensionsNormalizer.php
@@ -24,19 +24,8 @@ class DimensionsNormalizer
      */
     public function normalize(DimensionsInterface $dimensions)
     {
-        $values = [$dimensions->getLength()];
-
-        if ($this->math->greaterThan($dimensions->getWidth(), reset($values))) {
-            array_unshift($values, $dimensions->getWidth());
-        } else {
-            $values[] = $dimensions->getWidth();
-        }
-
-        if ($this->math->greaterThan($dimensions->getHeight(), reset($values))) {
-            array_unshift($values, $dimensions->getHeight());
-        } else {
-            $values[] = $dimensions->getHeight();
-        }
+        $values = [$dimensions->getLength(), $dimensions->getWidth(), $dimensions->getHeight()];
+        usort($values, [$this, 'sort']);
 
         $normalized = new Dimensions();
         $normalized->setUnit($dimensions->getUnit());
@@ -45,5 +34,18 @@ class DimensionsNormalizer
         $normalized->setHeight(next($values));
 
         return $normalized;
+    }
+
+    protected function sort($a, $b)
+    {
+        if ($this->math->eq($a, $b)) {
+            return 0;
+        }
+
+        if ($this->math->lessThan($a, $b)) {
+            return 1;
+        }
+
+        return -1;
     }
 }
---
+++ tests/Tool/DimensionsNormalizerTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace EsteIt\ShippingCalculator\Tests\Tool;
+
+use EsteIt\ShippingCalculator\Model\DimensionsInterface;
+use EsteIt\ShippingCalculator\Tool\DimensionsNormalizer;
+use Moriony\Trivial\Math\NativeMath;
+
+/**
+ * @group unit
+ */
+class DimensionsNormalizerTest extends \PHPUnit_Framework_TestCase
+{
+    protected $fixtures;
+
+    public function setUp()
+    {
+        $this->fixtures = null;
+    }
+
+    public function getFixture($name)
+    {
+        if (!$this->fixtures) {
+            $loader = new \Nelmio\Alice\Fixtures\Loader();
+            $this->fixtures = $loader->load(__DIR__.'/fixtures.yml');
+        }
+        return $this->fixtures[$name];
+    }
+
+    /**
+     * @dataProvider provideDimensions
+     */
+    public function testNormalize(DimensionsInterface $dimensions)
+    {
+        $math = new NativeMath();
+        $normalizer = new DimensionsNormalizer($math);
+        $result = $normalizer->normalize($dimensions);
+
+        $this->assertGreaterThanOrEqual($result->getWidth(), $result->getLength());
+        $this->assertGreaterThanOrEqual($result->getHeight(), $result->getWidth());
+        $this->assertSame($dimensions->getUnit(), $result->getUnit());
+    }
+
+    /**
+     * @return array
+     */
+    public function provideDimensions()
+    {
+        return [
+            [
+                $this->getFixture('dimensions_11_10_10_in'),
+            ],
+            [
+                $this->getFixture('dimensions_10_11_10_in'),
+            ],
+            [
+                $this->getFixture('dimensions_10_10_11_in'),
+            ],
+            [
+                $this->getFixture('dimensions_11_10_9_in'),
+            ],
+            [
+                $this->getFixture('dimensions_9_11_10_in'),
+            ],
+            [
+                $this->getFixture('dimensions_10_9_11_in'),
+            ],
+        ];
+    }
+}
